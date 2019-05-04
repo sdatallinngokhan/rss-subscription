9d3cf830283b198d225a9c6fa99a3623c6ec937b
gokhanpolat <gokhanpolat203@aof.anadolu.edu.tr>
2019-05-04 13:55:09 +0300

    login part is completed

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,12 @@
+import model.UserRoleType;
 import service.LoginService;
 import service.LoginServiceImpl;
 
 import java.util.Scanner;
 
 public class Application {
+
+    private static Scanner scanner = new Scanner(System.in);
 
     public static final String RSS_URL = "http://feeds.bbci.co.uk/news/world/rss.xml";
 
@@ -12,8 +15,8 @@ public class Application {
         System.out.println();
         System.out.println();
 
-        Scanner scanner = new Scanner(System.in);
         LoginService loginService = new LoginServiceImpl();
+        Application application = new Application();
 
         System.out.print("Your username : ");
         String username = scanner.nextLine();
@@ -24,8 +27,42 @@ public class Application {
 
         if (message.equals("You are not registered yet!!")) {
             System.out.println(message);
-        } else {
-            System.out.println("You are successfully logged in as " + message);
+            return;
         }
+
+        System.out.println("You successfully logged in as " + message);
+        System.out.println();
+        String option = application.showOptionsAndGetOption(message);
+
+        if (option.equals("wrong option")) {
+            System.out.println("You choice wrong option");
+            return;
+        }
+
+
+    }
+
+    private String showOptionsAndGetOption(String roleType) {
+        String option = "";
+        if (roleType.equals(UserRoleType.ADMIN.name()) || roleType.equals(UserRoleType.MODERATOR.name())) {
+            System.out.println("1. Get Feed List");
+            System.out.println("2. Add User");
+            System.out.println("3. Update User");
+            System.out.println("4. Delete User");
+            System.out.print("Your option? : ");
+            option = scanner.nextLine();
+        } else {
+            System.out.println("1. Get Feed List");
+            System.out.print("Your option? : ");
+            option = scanner.nextLine();
+        }
+
+        if (roleType.equals(UserRoleType.CLIENT.name()) && option.equals("1")) {
+            return option;
+        } else if (!roleType.equals(UserRoleType.CLIENT.name()) && (option.equals("1") || option.equals("2") || option.equals("3") || option.equals("4"))) {
+            return option;
+        }
+
+        return "wrong option";
     }
 }
